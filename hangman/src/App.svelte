9577ccb22a01e936bb2a904e42bf4eaa57329ee5
Igor <igor_motyka@mail.com>
2021-09-17 19:44:16 +0200

    Rectangle and basic styles ready

---
+++ hangman/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Gallow from "./Gallow.svelte";
+  import MissedLetters from "./MissedLetters.svelte";
+  import Word from "./Word.svelte";
+</script>
+
+<main>
+  <MissedLetters />
+  <Gallow />
+  <Word />
+  <div class="rectangle" />
+</main>
