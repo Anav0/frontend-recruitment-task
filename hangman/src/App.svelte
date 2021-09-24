1f3574e44a90b1a3a2da1f2a73277fddf99a9e64
Igor <igor_motyka@mail.com>
2021-09-24 12:37:13 +0200

    Added keypress event handler

--- hangman/src/App.svelte
+++ hangman/src/App.svelte
@@ -3,13 +3,22 @@
   import MissedLetters from "./MissedLetters.svelte";
   import Word from "./Word.svelte";
 
-  let missedLetters: string[] = ["a", "B", "D", "E"];
+  let missedLetters: string[] = [];
   let word: string = "ptyś";
+  let pressedKey = "";
+
+  function handleKeydown(e) {
+    if (e.code.includes("Key")) {
+      pressedKey = e.key.toUpperCase();
+    }
+  }
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
 <main>
   <MissedLetters {missedLetters} />
   <Gallow />
+  <PressedKey>{pressedKey}</PressedKey>
   <Word {word} />
   <div class="rectangle" />
 </main>
